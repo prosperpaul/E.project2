9da26d33936b66494d9cc8aea7e87f7acf4bfcfd
Chiamaka Prosper Nkwazema <149527450+prosperpaul@users.noreply.github.com>
2024-06-21 19:48:04 +0100

    Add files via upload

---
+++ routes/productRoutes.js
@@ -0,0 +1,29 @@
+const {
+    createProduct,
+    getAllProducts,
+    getSingleProduct,
+    updateProduct,
+    deleteSingleProduct
+} = require('../controllers/productController')
+
+const {
+    productAuth
+} = require('../middlewares/productAuth')
+
+const express = require('express')
+
+const Router = express.Router()
+
+
+Router.post('/create-product', productAuth, createProduct)
+
+Router.get('/product/:id', getSingleProduct)
+
+Router.get('/products', getAllProducts)
+
+Router.patch('/edit-product/:id', updateProduct)
+
+Router.delete('/delete-product/:id', deleteSingleProduct)
+
+
+module.exports = Router;
